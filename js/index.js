8eb5aadd32b391c7c7bb04b33d6bc9863e103d87
JamesHsu <aac219oo@gmail.com>
2023-06-21 20:28:12 +0800

    adjust layout and carousel done

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
     $('.carousel').slick({
-        dots: true,
+        dots: false,
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
+        //     prevArrow: '<button type="button" aria-label="Previous" class="slick-prev"><img src="./images/" alt="Previous"></button>',
+        // nextArrow: '<button type="button" aria-label="Next" class="slick-next"><img src="path/to/next-image.png" alt="Next"></button>',
     });
 });
