c49f83f84c35e67cfd85717a62468568d26a0707
JAMES\aac219oo <aac921oo@gmail.com>
2023-06-23 17:40:35 +0800

    scroll top and hover gotop img problem

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-$(document).ready(function () {
-    $('.carousel').slick({
-        dots: false,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        prevArrow:
-            '<button type="button" aria-label="Previous" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#3cacd3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17 20l-10-8 10-8"></path></svg></button>',
-        nextArrow:
-            '<button type="button" aria-label="Next" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#3cacd3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M7 20l10-8-10-8"></path></svg></button>',
-    });
+// $(document).ready(function () {
+$('.carousel').slick({
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow:
+        '<button type="button" aria-label="Previous" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#3cacd3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17 20l-10-8 10-8"></path></svg></button>',
+    nextArrow:
+        '<button type="button" aria-label="Next" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#3cacd3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M7 20l10-8-10-8"></path></svg></button>',
 });
+// });
